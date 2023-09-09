44325cfe4d37f23f7d34fb9dbd278c26ff791335
imhutao <92280773+imhutao@users.noreply.github.com>
2023-09-09 16:37:38 +1000

    Create print.py

---
+++ print.py
@@ -0,0 +1,17 @@
+print("pervaya stroka", sep="AAAAAAAAAAAA", end="/")
+print("vtoraya stroka")
+print(5+4)
+number = 10
+print(number)
+name = "terny"
+print("мой ник", name, "text")
+print(f"мой ник {name} text")
+a = 100
+b = 200
+print('slojenie', a+b)
+print('vichitanie', a-b)
+print('umnojenie', a*b)
+print('delenie', a/b)
+print('целостное деление', a//b)
+print('остатки от деления', a%b)
+
